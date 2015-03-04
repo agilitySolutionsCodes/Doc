377e8134b0cc7ce0959a10e6df6ba6de729cea5d
Yule Souza <yule.souza@agilitysolutions.com.br>
2015-03-04 16:31:56 -0300

    Commit arquivo SQL

---
+++ Banco/Carga.sql
@@ -0,0 +1,15 @@
+INSERT INTO usuario
+VALUES
+(
+	'Yule', 'Souza', 'yule.souza@agilitysolutions.com.br', GETDATE(), 'Y.jpg', GETDATE()
+)
+INSERT INTO perfil
+VALUES(
+	1, 'Gerente', GETDATE()
+)
+
+INSERT INTO senha
+VALUES(
+ 	1, 'Ldp6jRqNzk0= ', GETDATE()	
+)
+
